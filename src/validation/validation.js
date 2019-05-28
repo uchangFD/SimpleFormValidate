9189ad93635ce901e7a4b433b4c7ddb3ea285846
uchang.fd <uchang.fd@gmail.com>
2019-05-28 23:37:42 +0900

    UPDATE validation.js - 자료구조 변경

--- src/validation/validation.js
+++ src/validation/validation.js
@@ -2,27 +2,49 @@ import ValidationNode from "./validationNode";
 import assertType from "../utils/assertType";
 
 class Validation {
-  nodes = [];
+  nodes = {};
 
   getNode(name) {
     assertType(name, "string");
 
-    return this._findNode(name);
+    return this.nodes[name];
   }
   createNode(info) {
     // TODO: 빈 validationNode를 만들 것인가 말것인가?😩
-    this.nodes.push(new ValidationNode(info));
+    assertType(info, "object");
+
+    const propertiesThatMustExist = {
+      matcher: (value) => asserType(value, ["function", "regexp"]),
+      errorMsg: (value) => asserType(value, "string"),
+      name: (value) => asserType(value, "string"),
+    };
+
+    for (let [key, value] of Object.entries(info)) {
+      const assertPropertyType = propertiesThatMustExist[key];
+
+      if (typeof assertPropertyType === "undefined") {
+        throw new Error(`${key} is not property that must exist`);
+      }
+
+      assertPropertyType(value);
+    }
+
+    if (this.nodes[info.name]) {
+      throw new Error(`Aleady exist ${info.name} node`);
+    }
+
+    this.nodes[info.name] = new ValidationNode(info);
   }
   removeNode(name) {
     assertType(name, "string");
 
-    this.nodes = nodes.filter((node) => node.getName() !== name);
+    this.nodes[name] && delete this.nodes[name];
   }
   updateNode(name, nodeInfo) {
     assertType(name, "string");
     nodeInfo && assertType(nodeInfo, "object");
 
-    const node = this._findNode(name);
+    const node = this.nodes[name];
 
     if (!node) {
       // TODO: return할 것인가 예외처리 할 것인가?😩
@@ -34,10 +56,6 @@ class Validation {
   setMatcher(name, isAsync = false) {
     // TODO: Promise로 감싸는 부분은 체크할 때 하는걸로😁
     assertType(name, "string");
-  }
-
-  _findNode(name) {
-    return this.nodes.find((node) => node.getName() === name);
   }
 }
 
